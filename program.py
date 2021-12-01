5c7e6557206ece6837181c7b4d8cf506fe6d350c
Anton Andersson <apptillverkning@gmail.com>
2021-12-01 15:17:01 +0100

    Update program.py

--- program.py
+++ program.py
@@ -2,7 +2,7 @@
 
 depth_num = []
 prev_number = 0
-count_increse = 0
+count_inc = 0
 
 with open('input.txt') as f:
     lines = f.read().splitlines()
@@ -13,8 +13,8 @@ with open('input.txt') as f:
     for i in depth_num:
 
     	if prev_number < i:
-           		count_increse = count_increse + 1
+           		count_inc= count_inc + 1
     	prev_number = i
 
-print(count_increse - 1) # settings -1 to make sure it not counts the starting poinr
+print(count_inc - 1) # we dont want it to count the first number of the dive.
 
